e522ea66563ab9eb6e322a1d637b2985f6a3e25a
ian-afk <crkdosal@gmail.com>
2023-11-29 19:08:45 +0800

    create express app server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,22 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(200).send({ message: 'WELCOME TO EL AMAGROS EL' });
+});
+
+app.get('/employees', (req, res) => {
+  console.log(req);
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
