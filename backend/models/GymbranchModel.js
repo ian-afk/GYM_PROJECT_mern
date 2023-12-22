8f798ad6240f120690e45272ea016f30d0b620ec
ian-afk <crkdosal@gmail.com>
2023-12-22 21:44:45 +0800

    embed gymLocation

--- backend/models/GymbranchModel.js
+++ backend/models/GymbranchModel.js
@@ -14,16 +14,24 @@ const gymbranchSchema = new mongoose.Schema(
         message: 'gymLevel is either: junior,pro,delux',
       },
     },
-    address: {
-      type: String,
-      required: [true, 'gym address is required'],
-    },
+
     email: {
       type: String,
       required: [true, 'gym email is required'],
       unique: true,
       lowercase: true,
     },
+    gymLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    guides: Array,
     isDeleted: {
       type: Boolean,
       default: 0,
@@ -41,3 +49,19 @@ const gymbranchSchema = new mongoose.Schema(
 const GymBranch = mongoose.model('gymbranches', gymbranchSchema);
 
 export default GymBranch;
+
+/**
+ * Create Gym branch
+ * {
+    "gymLevel":"junior",
+    "address":"Bulacan",
+    "email":"bulacanamagrosss@email.com",
+    "gymLocation":{
+        "description":"Manila, Philippines",
+        "type":"Point",
+        "coordinates":[14.78629462113481, 121.07543091931124],
+        "address":"Quirino Hwy, San Jose del Monte City, 3023 Bulacan"
+    }
+
+}
+ */
