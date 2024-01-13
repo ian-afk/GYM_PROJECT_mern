4c351f73fa96d321f737affdcaf0389362ce4490
ian-afk <crkdosal@gmail.com>
2024-01-13 16:57:50 +0800

    setup of employee list

--- frontend/src/pages/Employee/EmployeeList.jsx
+++ frontend/src/pages/Employee/EmployeeList.jsx
@@ -1,3 +1,27 @@
 export default function EmployeeList() {
-  return <h1>Employee List</h1>;
+  return (
+    <>
+      <h1>Employee List</h1>
+      <table>
+        <thead>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Gender</th>
+          <th>Age</th>
+          <th>Address</th>
+          <th>Job</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+        </tbody>
+      </table>
+    </>
+  );
 }
