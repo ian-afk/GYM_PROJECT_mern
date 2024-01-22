cd91e3eb508930ba909ba8b5db92d43d0cf5ba2f
ian-afk <crkdosal@gmail.com>
2024-01-22 18:12:56 +0800

    Refactor browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import './home.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  Outlet,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 import EmployeeList from './pages/Employee/EmployeeList';
@@ -9,23 +17,30 @@ import NotFound from './components/NotFoundPage';
 import EmployeeCreate from './pages/Employee/EmployeeCreate';
 import ClientList from './pages/client/ClientList';
 export default function App() {
+  const AppLayout = () => (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />}></Route>
+
+        <Route path="/employees" element={<EmployeeList />}></Route>
+        <Route path="/employees/:id" element={<EmployeeView />}></Route>
+        <Route path="/employees/create" element={<EmployeeCreate />}></Route>
+
+        <Route path="/clients" element={<ClientList />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Route>
+    )
+  );
   return (
     <>
-      {/* <Home /> */}
-
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-
-          <Route path="/employees" element={<EmployeeList />}></Route>
-          <Route path="/employees/:id" element={<EmployeeView />}></Route>
-          <Route path="/employees/create" element={<EmployeeCreate />}></Route>
-
-          <Route path="/clients" element={<ClientList />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 }
