7a7875bcb7097ce61bc4714dd1546c03818e6f4f
ian-afk <crkdosal@gmail.com>
2024-02-08 21:30:27 +0800

    working with nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,36 +1,42 @@
 import { NavLink } from 'react-router-dom';
-
+import styles from './Nav.module.css';
+import { useState } from 'react';
 export default function Nav() {
+  const [operation, setOperation] = useState(false);
+
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to={'/'}>Home</NavLink>
         </li>
         <li>Profile</li>
-        <li>
+        <li
+          onClick={() => setOperation(!operation)}
+          className={operation ? 'active' : ''}
+        >
           Operations
-          <ul>
-            <li>
-              <NavLink to="/employees">Employees</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/clients'}>Clients</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/trainers'}>Trainers</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/schedules'}>Schedules</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/gymbranches'}>Gym branches</NavLink>
-            </li>
-            {/* <li>Members</li>
+        </li>
+        <ul style={{ display: operation ? '' : 'none' }}>
+          <li>
+            <NavLink to="/employees">Employees</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/clients'}>Clients</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/trainers'}>Trainers</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/schedules'}>Schedules</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/gymbranches'}>Gym branches</NavLink>
+          </li>
+          {/* <li>Members</li>
             <li>Payments</li>
             <li>Generate Report</li> */}
-          </ul>
-        </li>
+        </ul>
         <li>About us</li>
         <li>Contact us</li>
       </ul>
--- frontend/src/pages/Employee/EmployeeList.jsx
+++ frontend/src/pages/Employee/EmployeeList.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useAPIList } from '../../hooks/useAPIList';
 import RequestOptions from '../../utils/requestClass';
 export default function EmployeeList() {
