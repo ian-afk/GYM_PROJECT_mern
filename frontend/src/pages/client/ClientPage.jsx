e15ad135c7fcbdab7615334358cc812b31e0967c
ian-afk <crkdosal@gmail.com>
2024-03-01 23:39:43 +0800

    add navbar in Client module and css module  background

--- frontend/src/pages/client/ClientPage.jsx
+++ frontend/src/pages/client/ClientPage.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
-
+import Nav from '../../components/Nav';
+import styles from './ClientPage.module.css';
 function ClientPage() {
   return (
-    <div>
+    <div className={styles.clientContainer}>
+      <Nav />
       <Outlet />
     </div>
   );
