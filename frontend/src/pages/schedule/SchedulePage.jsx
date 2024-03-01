6be2c99f1612bc624888cbaaf8ba3278d66db1cd
ian-afk <crkdosal@gmail.com>
2024-03-01 23:57:41 +0800

    add nav component in schedule module page and add background styling

--- frontend/src/pages/schedule/SchedulePage.jsx
+++ frontend/src/pages/schedule/SchedulePage.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
-
+import Nav from '../../components/Nav';
+import styles from './SchedulePage.module.css';
 function SchedulePage() {
   return (
-    <div>
+    <div className={styles.schedContainer}>
+      <Nav />
       <Outlet />
     </div>
   );
