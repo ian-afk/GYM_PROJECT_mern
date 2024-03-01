e33995de8aab1fec4ae6b48f7c9fbd8975fb9f89
ian-afk <crkdosal@gmail.com>
2024-03-01 23:41:58 +0800

    add protect/authorization clientroute

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -6,10 +6,15 @@ import {
   getAllClient,
   getClient,
 } from '../controller/clientController.js';
+import { protect } from '../controller/authenticationController.js';
 
 const router = express.Router();
 
-router.route('/').get(getAllClient).post(createClient);
-router.route('/:id').get(getClient).patch(editClient).delete(deleteClient);
+router.route('/').get(protect, getAllClient).post(protect, createClient);
+router
+  .route('/:id')
+  .get(protect, getClient)
+  .patch(protect, editClient)
+  .delete(protect, deleteClient);
 
 export default router;
