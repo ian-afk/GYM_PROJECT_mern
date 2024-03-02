f5b73c5b3dd59330f1bce2edf42eb01c6731164c
ian-afk <crkdosal@gmail.com>
2024-03-02 16:50:16 +0800

    add protection authorization in schedule list module

--- frontend/src/pages/schedule/ScheduleList.jsx
+++ frontend/src/pages/schedule/ScheduleList.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useAPIList } from '../../hooks/useAPIList';
 import RequestOptions from '../../utils/requestClass';
+import { useAuth } from '../../context/AuthContext';
+import NotLoggedIn from '../../components/NotLoggedIn';
 
 export default function ScheduleList() {
+  const { token, isLoggedIn, setIsLoggedIn } = useAuth();
   const path = `schedules`;
   const {
     data: schedules,
     setData: setSchedules,
     isLoading,
     url,
-  } = useAPIList(path);
+    message,
+  } = useAPIList(path, token, setIsLoggedIn);
 
   function handleDelete(id) {
     const confirmed = window.confirm('Are you sure you want to delete?');
@@ -34,38 +38,48 @@ export default function ScheduleList() {
   }
   return (
     <>
-      <h1>Schedule List</h1>
-      <Link to={'/schedules/create'}>Create Schedule</Link>
-      {isLoading ? (
-        <h3>Loading...</h3>
+      {!isLoggedIn ? (
+        <NotLoggedIn message={message} />
       ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Trainer</th>
-              <th>Date</th>
-              <th>Time Start</th>
-              <th>Time End</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedules.map((sched) => (
-              <tr key={sched._id}>
-                <td>Trainer Name</td>
-                <td>{sched.startDate.slice(0, 10)}</td>
-                <td>{sched.timeStart.slice(11)}</td>
-                <td>{sched.timeEnd.slice(11)}</td>
-                <td>
-                  <Link to={`/schedules/${sched._id}`}>View</Link>
-                  <button type="button" onClick={() => handleDelete(sched._id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <>
+          {' '}
+          <h1>Schedule List</h1>
+          <Link to={'/schedules/create'}>Create Schedule</Link>
+          {isLoading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>Trainer</th>
+                  <th>Date</th>
+                  <th>Time Start</th>
+                  <th>Time End</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {schedules.map((sched) => (
+                  <tr key={sched._id}>
+                    <td>Trainer Name</td>
+                    <td>{sched.startDate.slice(0, 10)}</td>
+                    <td>{sched.timeStart.slice(11)}</td>
+                    <td>{sched.timeEnd.slice(11)}</td>
+                    <td>
+                      <Link to={`/schedules/${sched._id}`}>View</Link>
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(sched._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </>
       )}
     </>
   );
