5405d70482e004c13aead8e041efc653d93ecda7
ian-afk <crkdosal@gmail.com>
2024-03-04 23:27:12 +0800

    add navigation bar in gymbranch page and initial styling

--- frontend/src/pages/gymbranches/GymPage.jsx
+++ frontend/src/pages/gymbranches/GymPage.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
-
+import styles from './GymPage.module.css';
+import Nav from '../../components/Nav';
 function GymPage() {
   return (
-    <div>
+    <div className={styles.gymbranchContainer}>
+      <Nav />
       <Outlet />
     </div>
   );
