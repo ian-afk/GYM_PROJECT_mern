4720279f7c6605a7f3e0604d8aaa6a1ec130b77a
ian-afk <crkdosal@gmail.com>
2024-03-11 23:50:15 +0800

    add footer for login form

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -1,11 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import Nav from '../../components/Nav';
+import Footer from '../Home/Footer';
 import styles from './AuthPage.module.css';
 export default function AuthPage() {
   return (
-    <div className={styles.loginContainer}>
-      <Nav />
-      <Outlet className={styles.loginFormContainer} />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        background: '#929292',
+      }}
+    >
+      <div className={styles.loginContainer}>
+        <Nav />
+        <Outlet className={styles.loginFormContainer} />
+      </div>
+      <Footer style={{ alignSelf: 'flex-end' }} />
     </div>
   );
 }
