d6e1694fe35d430bec1776b1bd9f352e327b5f4a
ian-afk <crkdosal@gmail.com>
2024-03-14 23:15:37 +0800

    Remove home.module css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-import styles from './home.module.css';
 export default function Home() {
   return (
     <>
