34cadfacf6497a4e63c5b3806b0ecee4411a82c9
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-04-06 19:39:45 +0800

    fix spacing for the body

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
+import styles from './Home.module.css';
 export default function Home() {
   return (
-    <>
+    <div className={styles.mainHomeContainer}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
