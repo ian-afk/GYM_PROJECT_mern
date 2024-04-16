0d4165321f2c2dd5dd0cba19ddcae8606ca814eb
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-04-16 21:50:52 +0800

    set space between for login page and remove inline styling of div authpage and add it to authpage module

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -4,18 +4,11 @@ import Footer from '../Home/Footer';
 import styles from './AuthPage.module.css';
 export default function AuthPage() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        background: '#929292',
-      }}
-    >
+    <div className={styles.authPage}>
       <div className={styles.loginContainer}>
         <Nav />
-        <Outlet className={styles.loginFormContainer} />
       </div>
+      <Outlet className={styles.loginFormContainer} />
       <Footer style={{ alignSelf: 'flex-end' }} />
     </div>
   );
