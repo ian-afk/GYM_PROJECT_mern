831368ff8636eb4a5dc541e1cff5d7bef2432dd7
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-05-11 13:08:08 +0800

    add styling for linkbutton text color

--- frontend/src/components/LinkButtonComponent.jsx
+++ frontend/src/components/LinkButtonComponent.jsx
@@ -1,5 +1,9 @@
 import { Link } from 'react-router-dom';
 
 export default function LinkButtonComponent({ children, path }) {
-  return <Link to={path}>{children}</Link>;
+  return (
+    <Link style={{ color: '#5bc9c9' }} to={path}>
+      {children}
+    </Link>
+  );
 }
