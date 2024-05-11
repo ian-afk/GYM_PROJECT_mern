1b1a83955b89b8137799afedb11d10bbafff2515
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-05-11 13:07:33 +0800

    change background color and color to darkerwhite

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <div
       style={{ backgroundColor: '#366789', height: '60px', padding: '20px' }}
     >
-      <ul style={{ textAlign: 'center', listStyle: 'none', color: 'white' }}>
+      <ul style={{ textAlign: 'center', listStyle: 'none', color: '#e6e6e6' }}>
         <li>&copy; El Amagros EL 2024</li>
       </ul>
     </div>
