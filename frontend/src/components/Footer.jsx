64002c76937c679a4cfd8b4d5ef398bbbafd3277
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-05-11 12:51:50 +0800

    add empPage container for employee page holder and add new Component Footer for modules

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div
+      style={{ backgroundColor: '#366789', height: '60px', padding: '20px' }}
+    >
+      <ul style={{ textAlign: 'center', listStyle: 'none', color: 'white' }}>
+        <li>&copy; El Amagros EL 2024</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/Employee/EmployeePage.jsx
+++ frontend/src/pages/Employee/EmployeePage.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Nav from '../../components/Nav';
 import styles from '../Employee/EmployeePage.module.css';
+import Footer from '../../components/Footer';
 
 function EmployeePage() {
   return (
-    <div className={styles.empContainer}>
-      <Nav />
-      <Outlet />
+    <div className={styles.empPageContainer}>
+      <div className={styles.empContainer}>
+        <div>
+          <Nav />
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
